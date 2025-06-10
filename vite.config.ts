916985af092ddc11da73a071e5745ba4bf83ecfc
Lumban Sopian <lumban.sopian@lyteventures.com>
2025-06-10 14:57:59 +0700

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/resume/"
-})
-
+  base: "/resume/", // For https://lumbans.github.io/resume/
+  build: {
+    sourcemap: false, // Prevent /src/main.tsx requests
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+    minify: "esbuild",
+    cssCodeSplit: false,
+  },
+});
